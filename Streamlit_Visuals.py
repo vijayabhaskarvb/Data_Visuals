74b54b8b8381e0d9de7377ca9e527680de0cf3ff
Vijayabhaskar <141475400+vijayabhaskarvb@users.noreply.github.com>
2025-07-31 19:36:08 +0530

    Update Streamlit_Visuals.py

--- Streamlit_Visuals.py
+++ Streamlit_Visuals.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+sns.set()
+
 # Set page title and layout
 st.set_page_config(page_title="Adidas Data Visualizations")
 st.title("Adidas Dataset Visualizations")
