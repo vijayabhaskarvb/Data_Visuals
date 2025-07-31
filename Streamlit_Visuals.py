b29962b6e0c288cc320316773f526b4c32c6a8db
Vijayabhaskar <141475400+vijayabhaskarvb@users.noreply.github.com>
2025-07-31 16:31:48 +0530

    Update Streamlit_Visuals.py

--- Streamlit_Visuals.py
+++ Streamlit_Visuals.py
@@ -56,8 +56,8 @@ if st.checkbox("Show Pairplot (may be slow)"):
 
 #Pie Chart
 st.subheader("Pie Chart")
-selected_cat_col = st.selectbox("Select a categorical column for Pie Chart", categorical_cols)
-selected_num_col = st.selectbox("Select a numeric column to aggregate", numeric_cols, key='pie_numeric')
+selected_cat_col = st.multiselect("Select a categorical column for Pie Chart", categorical_cols,default=categorical_cols[4])
+selected_num_col = st.multiselect("Select a numeric column to aggregate", numeric_cols, key='pie_numeric', default=numerical_cols[3])
 
 if selected_cat_col and selected_num_col:
     pie_data = dataset.groupby(selected_cat_col)[selected_num_col].sum()
